447b2d9313aff66eb721d778e19a04938cf2b311
pnguye17 <peter.nguyen17@gmail.com>
2022-09-02 22:24:57 -0600

    added db driver

--- src/main/java/com/peter/Driver.java
+++ src/main/java/com/peter/Driver.java
@@ -1,9 +1,22 @@
 package com.peter;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("This is a maven project");
-	}
-
+		String url1 = "jdbc:mysql://localhost:3306/test1";
+        String user ="root";
+        String password = "secret";
+        try ( Connection connection = DriverManager.getConnection(url1, user, password)) {
+        	System.out.println("connected");
+        } catch ( SQLException e) {
+        	e.printStackTrace();
+        	System.out.println("error");
+        }
+        
+          
+}
 }
